58dad53347a8b730936c17e3a36382b9e4235990
Camilo Pineda <xtreme.c@hotmail.com>
2011-03-08 12:12:02 -0500

    odio a git

---
+++ src/Testall.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+
+
+public class Testall {
+ @ Test
+public void Probar () {
+	
+	MatematicaTest MT = new MatematicaTest ();
+	MT.testMinimo();
+}
+ @ Test
+ public void ProbarTodos () {
+		OrdenadorTest OT = new OrdenadorTest ();
+		OT.testOrdenar();
+	
+	}
+ 
+ 
+}
